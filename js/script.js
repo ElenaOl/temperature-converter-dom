f85e0f2cc9c3dfa8b29e3ad788eaadf3de657c56
ElenaOl <pravutiner@gmail.com>
2017-09-10 17:37:15 -0700

    js done, css to be completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
 console.log('Hello, front end');
+
+var onClick = function(e){
+  e.preventDefault();
+  var cRadio = document.getElementById("cRadioButton");
+  var tempElement = document.getElementById("temp-input");
+  var inputTemp = tempElement.value;
+  if (cRadio.checked){
+    var tempOutput = ((inputTemp - 32)*5)/9;
+    changeColor(tempOutput);
+  }else{
+    var tempOutput = (inputTemp *9/5 +32);
+    changeColor(inputTemp);
+  }
+  var result = "" +tempOutput;
+  document.getElementById("temp-output").innerText = result;
+};
+
+var changeColor = function(tempCelsius){
+  if (tempCelsius > 40){
+    document.getElementById("temp-output").style.backgroundColor = "red";
+  }else if(tempCelsius < 0){
+    document.getElementById("temp-output").style.backgroundColor = "blue";
+  }else{
+    document.getElementById("temp-output").style.backgroundColor = "";
+  }
+}
+
+var init = function(){
+  var submitButtonElement = document.getElementById("submitButton");
+  submitButtonElement.addEventListener("click", onClick);
+
+  var resetButtonElement = document.getElementById("resetButton");
+  resetButtonElement.addEventListener("click", function(){
+    document.getElementById("temp-output").innerText = "";
+    changeColor(20);
+  });
+}
+
+
+init();
+
+
+
+
+
+
+
